37630a91fde10428ec9fd2802bfb6873269e2bd3
Naim Miah <mmiah@fordham.edu>
2023-11-25 23:21:59 -0500

    half way done

---
+++ roblox.js
@@ -0,0 +1,11 @@
+// Given a 2D array keystrokes (first element: integer, encoded letter i.e. 0 – a, 1 – b, etc. Second element: integer, time it took to press key after previous key). Times sorted ascending order. Return the letter with the slowest time
+
+//Given array of weights and integer for max weight capacity, return the maximum amount of weight that can be made with the provided weights
+// Given [1, 3, 5], max = 7. Max weight is 6
+
+//Given 2D array of grid values for each row and a maximum value, find the max size square where the sum of the values is less than or equal to the maximum value. Ex: Given [ [1, 1, 1], [2, 2, 2], [ 3, 3, 3] ] and max value of 27, what is max size square?
+// 3 (because the sum of the values in this particular size 3 square is 18)
+
+// Determine unique users given a list of IP addresses and user pairs. A unique user is defined as a user whose name is not a prefix of another user's name at the same IP address.
+
+// Determine if a path is navigable given a 2D array of 1's and 0's given that 1's are walkable and 0's are not. The start is defined as the top left and the end is defined as the bottom right. The user may walk up, right, left, or down to walkable spaces or jump over one unwalkable space and land on walkable space.
